49503a9105474e65571ba2aea45e7837b9eb893a
Hans Pickelmann <code@pickelmann.eu>
2017-10-01 23:40:37 +0200

    changed tabs to spaces

--- src/main/java/com/geronimo/ehealth/EhealthApplication.java
+++ src/main/java/com/geronimo/ehealth/EhealthApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EhealthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EhealthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EhealthApplication.class, args);
+    }
 }
